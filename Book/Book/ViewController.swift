79ee4914fe3f83cc4b0b60aac61afb543fd8761a
administrator <administrator@6370.local>
2021-03-30 20:21:34 -0700

    ui tela login

--- Book/Book/ViewController.swift
+++ Book/Book/ViewController.swift
@@ -12,8 +12,9 @@ class ViewController: UIViewController {
     //MARK - IBOutlet
     
     @IBOutlet weak var usuarioTxt: UITextField?
+    @IBOutlet weak var senhaTxt: UITextField?
     
-    //MARK - IBOutlet style
+    //MARK - IBOutlet only style
     
     @IBOutlet weak var viewHeader: UIView?
     @IBOutlet weak var viewBtnEntrar: UIView?
@@ -22,6 +23,25 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         self.viewHeader?.layer.cornerRadius = 50
         self.viewBtnEntrar?.layer.cornerRadius = 25
+        
+        let imageView = UIImageView();
+        imageView.image = UIImage(named: "usuario.png");
+        
+        self.usuarioTxt?.layer.cornerRadius = 20
+        self.usuarioTxt?.layer.borderWidth = 1
+        self.usuarioTxt?.layer.borderColor = UIColor.systemGray5.cgColor
+        usuarioTxt?.leftView = imageView ;
+        usuarioTxt?.leftViewMode = UITextField.ViewMode.always
+        
+        let imageVieww = UIImageView();
+        imageVieww.image = UIImage(named: "senha.png");
+        
+        self.senhaTxt?.isSecureTextEntry = true;
+        self.senhaTxt?.layer.cornerRadius = 20
+        self.senhaTxt?.layer.borderWidth = 1
+        self.senhaTxt?.layer.borderColor = UIColor.systemGray5.cgColor
+        senhaTxt?.leftView = imageVieww ;
+        senhaTxt?.leftViewMode = UITextField.ViewMode.always
     }
 
 
